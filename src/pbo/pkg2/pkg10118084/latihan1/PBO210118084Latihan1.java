b86bf4145bcac80eb5bad830b038b05201e32eb7
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:13:02 +0700

    Add files via upload

---
+++ src/pbo/pkg2/pkg10118084/latihan1/PBO210118084Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg2.pkg10118084.latihan1;
+
+/**
+ *
+ * @author
+ * Nama  : Divi Adiffia Freza Alana
+*  Kelas : PBO2
+*  Nim   : 10118084
+*  Deskripsi Program : Program ini berisi program untuk menampilkan biodata
+
+ */
+public class PBO210118084Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------------");
+        System.out.println("NAMA\t\t: Divi Adiffia Freza Alana");
+        System.out.println("ALAMAT\t\t: Jalan Rereng barong no.19");
+        System.out.println("TEMPAT LAHIR\t: Indramayu");
+        System.out.println("TANGGAL LAHIR\t: 15 April 2000"+"\n" + "-------------------------");
+        System.out.println("HOBI\t\t: sport bike & teknologi");
+        System.out.println("MAKES\t\t: Nasi goreng & seafood");
+        System.out.println("MIKES\t\t: Susu Coklat");
+    }
+    
+}
